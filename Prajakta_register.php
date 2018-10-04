91113778d25337ac4543db4c771fe28977dda1bb
prajakta-neosoft <prajakta.mohite@neosofttech.com>
2018-10-04 19:25:46 +0530

    commit php file

---
+++ Prajakta_register.php
@@ -0,0 +1,33 @@
+<!Doctype html>
+<html>
+<head>
+ <title>Matrimony registration form!!!</title>
+ <body>
+  <form>
+   Matrimony profile for:<select name="profile">
+      <option value="myself">Self</option>
+         <option value="son">Son</option>
+         <option value="brother">Brother</option>
+         <option value="sister">Sister</option>
+         <option value="relative">Relative</option>
+         <option value="daughter">daughter</option>
+         <option value="friend">friend</option>
+         <select><br><br>
+          Name: <input type="text" name="applicantgivenname" required><br><br>
+          <input type="radio" name="gender" value="male" checked> Male  <input type="radio" name="gender" value="female"> Female  
+     <input type="radio" name="gender" value="other"> Other<br><br>
+     Date of Birth (dd-mm-yyyy): <input type="date" name="date"><br><br>
+     City: <select name="country">
+      <option value="india">india</option>
+         <option value="bangalore">Bangalore</option>
+         <option value="delhi">Delhi</option>
+         <option value="mumbai">Mumbai</option>
+         <option value="chennai">Chennai</option>
+         <option value="kerala">Kerala</option>
+         <option value="other">Other</option>
+         <select><br><br>
+  </form>
+ </body>
+
+ </head>
+</html>
